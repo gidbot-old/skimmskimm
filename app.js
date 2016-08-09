8f80c0377a91f258b592f621419521bbd0c62f51
gideon <gideon@blend.la>
2016-08-08 21:29:48 -0700

    add mail parser

---
+++ app.js
@@ -0,0 +1,23 @@
+var querystring = require('querystring');
+var MailParser = require("mailparser").MailParser;
+
+var server = require('http').createServer();
+server.addListener('request', function(req, res) {
+  var chunks = [];
+  req.on('data', chunks.push.bind(chunks));
+  req.on('end', function() {
+    var mailparser = new MailParser();
+    mailparser.on("end", function(mail_object) {
+      // TODO: use 'mail_object'
+      // see API for https://github.com/andris9/mailparser
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end();
+    });
+    var params = querystring.parse(chunks.join("").toString());
+    mailparser.write(params['message']);
+    mailparser.end();
+  });
+});
+var port = process.env.PORT || 3000;
+console.log(' [*] Listening on 0.0.0.0:' + port);
+server.listen(port, '0.0.0.0');
